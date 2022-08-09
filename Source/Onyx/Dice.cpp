c97835135575ed344917cb4e46e0f30f5f0ff7cb
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2022-08-09 18:38:35 -0400

    Refactor Dice into samller functions

--- Source/Onyx/Dice.cpp
+++ Source/Onyx/Dice.cpp
@@ -1,12 +1,21 @@
 #include "Dice.h"
 
+bool IsValidDiceCount(int dice_count)
+{
+	return dice_count <= 0 || dice_count >= 10;
+}
+
+void HandleInvalidDiceCount(int dice_count)
+{
+	if(IsValidDiceCount(dice_count))
+	{
+		throw(FString("Roll cd1 is not valid."));
+	}
+}
+
 int Roll(FString n)
 {
-	int dice_count = n[0] - '0';
-	if(dice_count <= 0 || dice_count >= 10)
-	{
-		FString errMsg = "Roll cd1 is not valid.";
-		throw(errMsg);
-	}
-	return dice_count;
+	const int Dice_Count = n[0] - '0';
+	HandleInvalidDiceCount(Dice_Count);
+	return Dice_Count;
 }
--- Source/Onyx/Dice.spec.cpp
+++ Source/Onyx/Dice.spec.cpp
@@ -2,27 +2,29 @@
 #include <Onyx/Dice.h>
 
 DEFINE_SPEC(DiceSpec, "Onyx.Dice", EAutomationTestFlags::ProductFilter | EAutomationTestFlags::ApplicationContextMask)
+
 void DiceSpec::Define()
 {
-	Describe("A valid roll", [this] ()
+	Describe("A valid roll", [this]()
 	{
 		It("should return possible range", [this]()
 		{
-		  TestTrue("1", Roll("1d1") == 1);
+			TestTrue("1", Roll("1d1") == 1);
 		});
 	});
-	
-	Describe("An invalid roll input", [this] ()
+
+	Describe("An invalid roll input", [this]()
 	{
-		It("should return exception", [this] ()
+		It("should return exception", [this]()
 		{
 			FString invalidInput = "cd1";
 			try
 			{
-			  Roll(invalidInput);
-			} catch (FString error)
+				Roll(invalidInput);
+			}
+			catch (FString error)
 			{
-				FString expectedError = "Roll " + invalidInput + " is not valid."; 
+				FString expectedError = "Roll " + invalidInput + " is not valid.";
 				TestEqual("Expected Error", error, expectedError);
 			}
 		});
