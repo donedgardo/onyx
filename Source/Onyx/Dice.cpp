911ac7013853670675fdd1d88b43ee6b2eb91d99
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2022-08-10 01:14:25 -0400

    Refactor implementation of roll

--- Source/Onyx/Dice.cpp
+++ Source/Onyx/Dice.cpp
@@ -1,7 +1,5 @@
 #include "Dice.h"
 
-#include <string>
-
 bool IsInvalidDiceCount(const int DiceCount)
 {
 	return DiceCount <= 0 || DiceCount >= 10;
@@ -12,7 +10,7 @@ bool IsInvalidSideCount(const int SideCount)
 	return SideCount <= 0 || SideCount >= 10;
 }
 
-void HandleInvalidDiceCount(FString input)
+void HandleInvalidRollInput(FString input)
 {
 	throw FString("Roll " + input + " is not valid.");
 }
@@ -23,25 +21,38 @@ int RandomNonZeroNumber(const int InclusiveMax)
 	return rand() % InclusiveMax + 1;
 }
 
-int Roll(FString RollInput)
+struct FRoll
+{
+	int Dice;
+	int Side;
+};
+
+FRoll GetDiceAndSideCount(FString RollInput)
 {
 	TArray<FString> DiceAndSide;
 	RollInput.ParseIntoArray(DiceAndSide, TEXT("d"), true);
 	if (DiceAndSide.Num() != 2)
 	{
-		HandleInvalidDiceCount(RollInput);
+		HandleInvalidRollInput(RollInput);
 	}
-	const int DiceCount = FCString::Atoi(*DiceAndSide[0]);
-	const int SideCount = FCString::Atoi(*DiceAndSide[1]);
-	if (IsInvalidDiceCount(DiceCount) || IsInvalidSideCount(SideCount))
+	return FRoll{
+		FCString::Atoi(*DiceAndSide[0]),
+		FCString::Atoi(*DiceAndSide[1])
+	};
+}
+
+int Roll(FString RollInput)
+{
+	FRoll rollInput = GetDiceAndSideCount(RollInput);
+	if (IsInvalidDiceCount(rollInput.Dice) || IsInvalidSideCount(rollInput.Side))
 	{
-		HandleInvalidDiceCount(RollInput);
+		HandleInvalidRollInput(RollInput);
 	}
 
 	int roll = 0;
-	for (int i = 0; i < DiceCount; ++i)
+	for (int i = 0; i < rollInput.Dice; ++i)
 	{
-		roll += RandomNonZeroNumber(SideCount);
+		roll += RandomNonZeroNumber(rollInput.Side);
 	}
 	return roll;
 }
