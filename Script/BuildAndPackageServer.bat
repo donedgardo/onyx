b1e0c0cc65c5aec3803a2e2332b75aa33f1d867a
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2022-08-18 21:23:07 -0400

    Add server config

--- Script/BuildAndPackageServer.bat
+++ Script/BuildAndPackageServer.bat
@@ -3,4 +3,4 @@ set workspace=%~2
 set projectFilename=%~3
 
 
-"%engineLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%workspace%\%projectFilename%" -cook -targetplatform=Server -package -noP4 -platform=Win64 -clientconfig=Development -build -unattended -archive -distribution -ut8output -archivedirectory="%WORKSPACE%/temp/Development/x64" -pak -nodebuginfo
+"%engineLocation%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%workspace%\%projectFilename%" -cook -server -package -noP4 -platform=Win64 -clientconfig=Development -build -unattended -archive -distribution -ut8output -archivedirectory="%WORKSPACE%/temp/Development/x64" -pak -nodebuginfo
